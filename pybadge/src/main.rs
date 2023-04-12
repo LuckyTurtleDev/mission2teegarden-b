d92ac02645990fa36d06989f5990dbf25e96ddfd
LuckyTurtleDev <44570204+LuckyTurtleDev@users.noreply.github.com>
2023-04-12 19:42:08 +0200

    add ci (#1)

--- pybadge/src/main.rs
+++ pybadge/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use pybadge::PyBadge;
+use pybadge_high as pybadge;
+use pybadge_high::prelude::*;
+
+#[entry]
+fn main() -> ! {
+	let mut pybadge = PyBadge::take().unwrap();
+	loop {
+		pybadge.red_led.on().unwrap();
+		pybadge.delay.delay_ms(1000_u16);
+		pybadge.red_led.off().unwrap();
+		pybadge.delay.delay_ms(1000_u16);
+	}
 }
