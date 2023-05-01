1492b67921b1d0b87c63ebba813960ff4bd6bf69
luckyturtledev <git@lukas1818.de>
2023-05-01 21:58:09 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -9,59 +9,56 @@
 
 use core::fmt::Write;
 use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyle},
-    prelude::*,
-    text::Text,
+	mono_font::{ascii::FONT_6X10, MonoTextStyle},
+	prelude::*,
+	text::Text
 };
 use heapless::String;
-use pybadge::time::uptime;
-use pybadge::Display;
-use pybadge::NeoPixelColor;
-use pybadge::{prelude::*, Color, PyBadge};
+use pybadge::{prelude::*, time::uptime, Color, Display, NeoPixelColor, PyBadge};
 use pybadge_high as pybadge;
 
 const NUM_LEDS: u8 = 5;
 
 fn draw(display: &mut Display, time: u32) {
-    let mut string = String::<32>::new();
-    let style = MonoTextStyle::new(&FONT_6X10, Color::BLACK);
-    string.clear();
-    write!(string, "Zeit: {} ms", time).unwrap();
-    display.clear(Color::GREEN).unwrap();
-    Text::new(&string, Point::new(20, 30), style)
-        .draw(display)
-        .ok();
+	let mut string = String::<32>::new();
+	let style = MonoTextStyle::new(&FONT_6X10, Color::BLACK);
+	string.clear();
+	write!(string, "Zeit: {} ms", time).unwrap();
+	display.clear(Color::GREEN).unwrap();
+	Text::new(&string, Point::new(20, 30), style)
+		.draw(display)
+		.ok();
 }
 
 #[entry]
 fn main() -> ! {
-    let mut pybadge = PyBadge::take().unwrap();
-    let mut display = pybadge.display;
-    let mut buttons = pybadge.buttons;
-    let mut timestamp;
-    let mut button_a_pressed = false;
+	let mut pybadge = PyBadge::take().unwrap();
+	let mut display = pybadge.display;
+	let mut buttons = pybadge.buttons;
+	let mut timestamp;
+	let mut button_a_pressed = false;
 
-    loop {
-        while button_a_pressed == buttons.a_pressed() {
-            buttons.update();
-        }
+	loop {
+		while button_a_pressed == buttons.a_pressed() {
+			buttons.update();
+		}
 
-        if buttons.a_pressed() {
-            pybadge
-                .neopixel
-                .write((0..NUM_LEDS).map(|_i| NeoPixelColor { r: 0, g: 2, b: 0 }))
-                .unwrap();
-            timestamp = uptime();
-            display.clear(Color::GREEN).unwrap();
-            let duration = uptime().0 - timestamp.0;
-            draw(&mut display, duration)
-        } else {
-            pybadge
-                .neopixel
-                .write((0..NUM_LEDS).map(|_i| NeoPixelColor { r: 0, g: 0, b: 0 }))
-                .unwrap();
-            display.clear(Color::BLACK).unwrap();
-        }
-        button_a_pressed = !button_a_pressed;
-    }
+		if buttons.a_pressed() {
+			pybadge
+				.neopixel
+				.write((0..NUM_LEDS).map(|_i| NeoPixelColor { r: 0, g: 2, b: 0 }))
+				.unwrap();
+			timestamp = uptime();
+			display.clear(Color::GREEN).unwrap();
+			let duration = uptime().0 - timestamp.0;
+			draw(&mut display, duration)
+		} else {
+			pybadge
+				.neopixel
+				.write((0..NUM_LEDS).map(|_i| NeoPixelColor { r: 0, g: 0, b: 0 }))
+				.unwrap();
+			display.clear(Color::BLACK).unwrap();
+		}
+		button_a_pressed = !button_a_pressed;
+	}
 }
