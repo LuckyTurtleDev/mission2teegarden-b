20867de49550e9696e404d5a90099483bd782bbd
LuckyTurtleDev <git@lukas1818.de>
2023-05-07 17:52:46 +0200

    move assets; adjust tiled map + tileset to new grafics

--- pc/assets/img/BaseTiles/BaseTiles.tsx
+++ pc/assets/img/BaseTiles/BaseTiles.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="BaseTile" tilewidth="128" tileheight="128" tilecount="2" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="1">
-  <image width="128" height="128" source="grass.png"/>
+ <tile id="0">
+  <image width="256" height="256" source="grass.png"/>
  </tile>
- <tile id="2">
-  <image width="128" height="128" source="puddle.png"/>
+ <tile id="1">
+  <image width="256" height="256" source="stone.png"/>
  </tile>
 </tileset>
