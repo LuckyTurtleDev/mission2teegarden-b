174df9380920c62a4aabab4efd414a9be51f7822
luckyturtledev <git@lukas1818.de>
2023-05-12 12:04:59 +0200

    start object layer

--- map/src/lib.rs
+++ map/src/lib.rs
@@ -4,13 +4,14 @@ use thiserror::Error;
 use tiled::{LayerType, Loader};
 
 pub mod tiles;
-use tiles::{InvalidTileID, MapBaseTile};
+use tiles::{InvalidTileID, MapBaseTile, ObjectTile};
 
 #[derive(Clone, Debug, SelfRustTokenize)]
 pub struct Map {
 	pub width: u8,
 	pub height: u8,
-	pub base_layer: Vec<Vec<MapBaseTile>>
+	pub base_layer: Vec<Vec<MapBaseTile>>,
+	pub object_layer: Vec<Vec<Option<ObjectTile>>>
 }
 
 #[derive(Error, Debug)]
@@ -32,11 +33,13 @@ pub enum MapError {
 }
 
 impl Map {
+	// this is ugly. Should i refactor this?
 	pub fn from_tmx(path: impl AsRef<Path>) -> Result<Self, MapError> {
 		let map = Loader::new().load_tmx_map(path)?;
 		let width: u8 = map.width.try_into().map_err(|_| MapError::ToWidth)?;
 		let height: u8 = map.height.try_into().map_err(|_| MapError::ToHight)?;
 		let mut base_layer = Vec::with_capacity(height as usize);
+		let mut object_layer = Vec::with_capacity(height as usize);
 		for (i, layer) in map.layers().enumerate() {
 			match i {
 				0 => match layer.layer_type() {
@@ -55,13 +58,30 @@ impl Map {
 					},
 					_ => return Err(MapError::WrongLayer(i, "TileLayer".to_owned()))
 				},
+				1 => match layer.layer_type() {
+					LayerType::Tiles(tile_layer) => {
+						for x in 0..width {
+							let mut column = Vec::with_capacity(width as usize);
+							for y in 0..height {
+								let tile = match tile_layer.get_tile(x.into(), y.into()) {
+									Some(tile) => Some(ObjectTile::try_from(tile.id())?),
+									None => None
+								};
+								column.push(tile);
+							}
+							object_layer.push(column);
+						}
+					},
+					_ => return Err(MapError::WrongLayer(i, "TileLayer".to_owned()))
+				},
 				_ => return Err(MapError::ToManyLayers)
 			}
 		}
 		Ok(Map {
 			width,
 			height,
-			base_layer
+			base_layer,
+			object_layer
 		})
 	}
 
@@ -72,6 +92,16 @@ impl Map {
 				.iter()
 				.enumerate()
 				.map(move |(y, item)| (x as u8, y as u8, item))
+		})
+	}
+
+	/// return an iterator over all ObjectTiles and its x and y postion
+	pub fn iter_object_layer(&self) -> impl Iterator<Item = (u8, u8, ObjectTile)> + '_ {
+		self.object_layer.iter().enumerate().flat_map(|(x, y_vec)| {
+			y_vec
+				.iter()
+				.enumerate()
+				.filter_map(move |(y, item)| item.map(|item| (x as u8, y as u8, item)))
 		})
 	}
 }
--- map/src/tiles.rs
+++ map/src/tiles.rs
@@ -2,6 +2,12 @@ use num_enum::TryFromPrimitive;
 use self_rust_tokenize::SelfRustTokenize;
 use thiserror::Error;
 
+#[derive(Debug, Copy, Clone, Error)]
+pub enum InvalidTileID {
+	#[error("invalid tiel id {0}")]
+	InvalidId(u32)
+}
+
 ///Store all Tiles, with can be used at the map background
 #[derive(
 	Clone, Copy, Debug, Default, Eq, SelfRustTokenize, PartialEq, TryFromPrimitive,
@@ -10,19 +16,30 @@ use thiserror::Error;
 pub enum MapBaseTile {
 	//numbers must match them from the Tiled tilesets
 	#[default]
-	Grass = 0,
-	Stone = 1
-}
-
-#[derive(Debug, Copy, Clone, Error)]
-pub enum InvalidTileID {
-	#[error("invalid tiel id {0}")]
-	InvalidId(u32)
+	Grass = 0
 }
 
 impl TryFrom<u32> for MapBaseTile {
 	type Error = InvalidTileID;
 	fn try_from(value: u32) -> Result<MapBaseTile, Self::Error> {
+		let value_u8: u8 = value
+			.try_into()
+			.map_err(|_| Self::Error::InvalidId(value))?;
+		Self::try_from_primitive(value_u8).map_err(|_| Self::Error::InvalidId(value))
+	}
+}
+
+///Store all Tiles, with can be place the layer above the background
+#[derive(Clone, Copy, Debug, Eq, SelfRustTokenize, PartialEq, TryFromPrimitive)]
+#[repr(u8)]
+pub enum ObjectTile {
+	//numbers must match them from the Tiled tilesets
+	Stone = 0
+}
+
+impl TryFrom<u32> for ObjectTile {
+	type Error = InvalidTileID;
+	fn try_from(value: u32) -> Result<ObjectTile, Self::Error> {
 		let value_u8: u8 = value
 			.try_into()
 			.map_err(|_| Self::Error::InvalidId(value))?;
