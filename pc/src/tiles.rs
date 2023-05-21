b17a55509ebdc0cc82be0ffe32ee48a669b3ed99
LuckyTurtleDev <git@lukas1818.de>
2023-05-21 13:23:48 +0200

    migrate to Macroquad from Tetra (#19)

--- pc/src/tiles.rs
+++ pc/src/tiles.rs
@@ -1,113 +1,98 @@
-use crate::GetTexture;
 pub use m3_map::tiles::MapBaseTile;
 use m3_map::tiles::{ObjectTile, PlayerTile, Tile};
-use tetra::{graphics::Texture, Context};
+use macroquad::{prelude::ImageFormat, texture::Texture2D};
+use once_cell::sync::Lazy;
+
+pub static TEXTURES: Lazy<Textures> = Lazy::new(|| Textures::init());
+
+pub trait GetTexture {
+	fn texture(&self, textures: &Textures) -> Texture2D;
+}
 
 ///Store all Textures
 pub struct Textures {
-	grass: Texture,
-	stone: Texture,
-	player1_car: Texture,
-	player2_car: Texture,
-	player3_car: Texture,
-	player4_car: Texture,
-	global_goal: Texture
+	grass: Texture2D,
+	stone: Texture2D,
+	player1_car: Texture2D,
+	player2_car: Texture2D,
+	player3_car: Texture2D,
+	player4_car: Texture2D,
+	global_goal: Texture2D
 }
 
 impl Textures {
 	///init all Textures
-	pub fn init(ctx: &mut Context) -> Textures {
+	pub fn init() -> Textures {
 		Textures {
-			grass: Texture::from_encoded(
-				ctx,
-				include_bytes!("../assets/img/BaseTiles/grass.png")
+			grass: Texture2D::from_file_with_format(
+				include_bytes!("../assets/img/BaseTiles/grass.png"),
+				Some(ImageFormat::Png)
+			),
+			stone: Texture2D::from_file_with_format(
+				include_bytes!("../assets/img/ObjectTiles/stone.png"),
+				Some(ImageFormat::Png)
+			),
+			player1_car: Texture2D::from_file_with_format(
+				include_bytes!("../assets/img/Player/player1_car.png"),
+				Some(ImageFormat::Png)
+			),
+			player2_car: Texture2D::from_file_with_format(
+				include_bytes!("../assets/img/Player/player2_car.png"),
+				Some(ImageFormat::Png)
+			),
+			player3_car: Texture2D::from_file_with_format(
+				include_bytes!("../assets/img/Player/player3_car.png"),
+				Some(ImageFormat::Png)
+			),
+			player4_car: Texture2D::from_file_with_format(
+				include_bytes!("../assets/img/Player/player4_car.png"),
+				Some(ImageFormat::Png)
+			),
+			global_goal: Texture2D::from_file_with_format(
+				include_bytes!("../assets/img/Player/goal.png"),
+				Some(ImageFormat::Png)
 			)
-			.unwrap(),
-			stone: Texture::from_encoded(
-				ctx,
-				include_bytes!("../assets/img/ObjectTiles/stone.png")
-			)
-			.unwrap(),
-			player1_car: Texture::from_encoded(
-				ctx,
-				include_bytes!("../assets/img/Player/player1_car.png")
-			)
-			.unwrap(),
-			player2_car: Texture::from_encoded(
-				ctx,
-				include_bytes!("../assets/img/Player/player2_car.png")
-			)
-			.unwrap(),
-			player3_car: Texture::from_encoded(
-				ctx,
-				include_bytes!("../assets/img/Player/player3_car.png")
-			)
-			.unwrap(),
-			player4_car: Texture::from_encoded(
-				ctx,
-				include_bytes!("../assets/img/Player/player4_car.png")
-			)
-			.unwrap(),
-			global_goal: Texture::from_encoded(
-				ctx,
-				include_bytes!("../assets/img/Player/goal.png")
-			)
-			.unwrap()
 		}
 	}
 }
 
-impl<'a> GetTexture<'a> for MapBaseTile {
+impl GetTexture for MapBaseTile {
 	///get Texture assioated with this Tile
-	fn texture(&self, textures: &'a Textures) -> &'a Texture {
+	fn texture(&self, textures: &Textures) -> Texture2D {
 		match self {
-			Self::Grass => &textures.grass
+			Self::Grass => textures.grass
 		}
 	}
 }
 
-impl<'a> GetTexture<'a> for ObjectTile {
+impl GetTexture for ObjectTile {
 	///get Texture assioated with this Tile
-	fn texture(&self, textures: &'a Textures) -> &'a Texture {
+	fn texture(&self, textures: &Textures) -> Texture2D {
 		match self {
-			Self::Stone => &textures.stone
+			Self::Stone => textures.stone
 		}
 	}
 }
 
-impl<'a> GetTexture<'a> for PlayerTile {
+impl GetTexture for PlayerTile {
 	///get Texture assioated with this Tile
-	fn texture(&self, textures: &'a Textures) -> &'a Texture {
+	fn texture(&self, textures: &Textures) -> Texture2D {
 		match self {
-			Self::Car1 => &textures.player1_car,
-			Self::Car2 => &textures.player1_car,
-			Self::Car3 => &textures.player1_car,
-			Self::Car4 => &textures.player1_car,
-			Self::GlobalGoal => &textures.global_goal
+			Self::Car1 => textures.player1_car,
+			Self::Car2 => textures.player1_car,
+			Self::Car3 => textures.player1_car,
+			Self::Car4 => textures.player1_car,
+			Self::GlobalGoal => textures.global_goal
 		}
 	}
 }
 
-impl<'a> GetTexture<'a> for Tile {
-	fn texture(&self, textures: &'a Textures) -> &'a Texture {
+impl GetTexture for Tile {
+	fn texture(&self, textures: &Textures) -> Texture2D {
 		match self {
 			Tile::MapBaseTile(tile) => tile.texture(textures),
 			Tile::MapObjectTile(tile) => tile.texture(textures),
 			Tile::PlayerTile(tile) => tile.texture(textures)
 		}
-	}
-}
-
-#[cfg(test)]
-mod tests {
-	use crate::tiles::Textures;
-	use tetra::ContextBuilder;
-
-	#[test]
-	#[ignore] //can only run if gui is aviable. Ignore it, so ci does not fail
-	///verify that all image files are valid and supported
-	fn texture_init() {
-		let mut ctx = ContextBuilder::new("test", 640, 480).build().unwrap();
-		Textures::init(&mut ctx);
 	}
 }
