8c28a6732b5c543bbd6c73eb056c22e6e5663fe3
luckyturtledev <git@lukas1818.de>
2023-06-01 19:05:10 +0200

    draw old solution

--- pybadge/src/activitys/card_selecter.rs
+++ pybadge/src/activitys/card_selecter.rs
@@ -136,6 +136,17 @@ pub(crate) fn init(state: &mut State<'_>) {
 		draw_count(i as u8, count, &mut state.display, state.text_style_large);
 	}
 	state.cursor = (0, 1);
+	// draw (old) solution
+	// (if this is a retry the player has still a solution from the last attempt)
+	for (i, card) in state.solution.iter().enumerate() {
+		draw_card(
+			i,
+			1,
+			Some(card),
+			&mut state.display,
+			state.text_style_on_card
+		);
+	}
 }
 
 pub(crate) fn update(state: &mut State<'_>) {
