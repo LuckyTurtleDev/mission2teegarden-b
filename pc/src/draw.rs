db568e1c1056c3067fa476e77d7b87b3beffd092
Georg Thiesen <g.thiesen@posteo.de>
2023-06-02 09:26:54 +0200

    WIP: player_movement (#35)
    * started implementing of actual movement calculation
    
    * adapt GameState to new Implementation
    
    * adapt draw method
    
    * getRelativXY reworked
    
    * changed player position type from u8 to i8
    
    * it works? (position var back to u8)
    
    * fmt
    
    * fmt
    
    * init player movement
    
    * started updating player position
    
    * problem with update_player_position
    
    * moved update code in draw method
    
    * foward movement works
    
    * Cars can take turns now, not completely correct
    
    * everything commited
    
    * fmt, cars drift around corner

--- pc/src/draw.rs
+++ pc/src/draw.rs
@@ -1,4 +1,4 @@
-use crate::{tiles::GetTexture, GameState, TEXTURES};
+use crate::{tiles::GetTexture, GameState, Orientation, RotationPoint, TEXTURES};
 use macroquad::{math::Vec2, prelude::*};
 
 impl GameState {
@@ -14,9 +14,9 @@ impl GameState {
 				let dest_size = (screen_width / game_run.level.width as f32)
 					.min(screen_height / game_run.level.height as f32);
 				//center map, by using offset
-				let offset_x =
+				let map_offset_x =
 					(screen_width - dest_size * game_run.level.width as f32) / 2.0;
-				let offset_y =
+				let map_offset_y =
 					(screen_height - dest_size * game_run.level.height as f32) / 2.0;
 				for (x, y, tile) in game_run.level.iter_all() {
 					let texture = tile.texture(&TEXTURES);
@@ -26,13 +26,93 @@ impl GameState {
 					};
 					draw_texture_ex(
 						texture,
-						x as f32 * dest_size + offset_x,
-						y as f32 * dest_size + offset_y,
+						x as f32 * dest_size + map_offset_x,
+						y as f32 * dest_size + map_offset_y,
 						//This param can filter colors.
 						//Set every value to 1 to keep all colors, by using WHITE
 						WHITE,
 						draw_params
 					);
+				}
+
+				//draw players
+				let player_textures = TEXTURES.get_player_textures();
+				for (x, player) in game_run.level.iter_player().enumerate() {
+					let texture = player_textures[x];
+					// Car drives forward
+					if game_run.player_states[x].rotation.pivot
+						== RotationPoint::NoRotation
+					{
+						let relative_pos_x = (game_run.player_states[x].position.0
+							as f32 - player.position.0 as f32)
+							* dest_size / (self.movement_time
+							/ self.delta_time);
+						let relative_pos_y = (game_run.player_states[x].position.1
+							as f32 - player.position.1 as f32)
+							* dest_size / (self.movement_time
+							/ self.delta_time);
+						let rotation: f32 = match game_run.player_states[x].orientation {
+							Orientation::North => 0.0,
+							Orientation::East => 90.0,
+							Orientation::South => 180.0,
+							Orientation::West => 270.0
+						};
+						let draw_params = DrawTextureParams {
+							dest_size: Some(Vec2::new(dest_size, dest_size)),
+							rotation: rotation.to_radians(),
+							..Default::default()
+						};
+						draw_texture_ex(
+							texture,
+							player.position.0 as f32 * dest_size
+								+ relative_pos_x + map_offset_x,
+							player.position.1 as f32 * dest_size
+								+ relative_pos_y + map_offset_y,
+							WHITE,
+							draw_params
+						);
+					// Car makes a turn
+					} else {
+						let pivot_xy = match game_run.player_states[x].rotation.pivot {
+							RotationPoint::TopLeft => Vec2::new(
+								player.position.0 as f32,
+								(player.position.1) as f32
+							),
+							RotationPoint::TopRight => Vec2::new(
+								(player.position.0 + 1) as f32,
+								player.position.1 as f32
+							),
+							RotationPoint::BottomLeft => Vec2::new(
+								player.position.0 as f32,
+								(player.position.1 + 1) as f32
+							),
+							RotationPoint::BottomRight => Vec2::new(
+								(player.position.0 + 1) as f32,
+								(player.position.1 + 1) as f32
+							),
+							// Never happens
+							RotationPoint::NoRotation => Vec2::new(0.0, 0.0)
+						};
+						let pivot_xy = Vec2::new(
+							pivot_xy.x * dest_size + map_offset_x,
+							pivot_xy.y * dest_size + map_offset_y
+						);
+						let angle: f32 = 90.0 / (self.movement_time / self.delta_time)
+							* game_run.player_states[x].rotation.sign as f32;
+						let draw_params = DrawTextureParams {
+							dest_size: Some(Vec2::new(dest_size, dest_size)),
+							rotation: angle.to_radians(),
+							pivot: Some(pivot_xy),
+							..Default::default()
+						};
+						draw_texture_ex(
+							texture,
+							player.position.0 as f32 * dest_size + map_offset_x,
+							player.position.1 as f32 * dest_size + map_offset_y,
+							WHITE,
+							draw_params
+						);
+					}
 				}
 			}
 		}
--- pc/src/update.rs
+++ pc/src/update.rs
@@ -1,11 +1,24 @@
-use m3_map::Orientation;
+use std::borrow::BorrowMut;
 
-use crate::{cards_ev::CarAction, GameState, PlayerState};
+use m3_map::{Orientation, Player};
+use macroquad::prelude::*;
+
+use crate::{
+	cards_ev::CarAction,
+	tiles::{GetTexture, Textures, TEXTURES},
+	GameState, PlayerState, Rotation, RotationPoint
+};
 
 impl GameState {
 	///update the current state.
 	pub async fn update(&mut self) {
 		let _player_events = self.input_players.get_events();
+		if self.delta_time >= self.movement_time {
+			self.delta_time -= self.movement_time;
+
+			self.next_move();
+		}
+		self.delta_time += get_frame_time();
 
 		//use delta time, to avoid that the logic is effected by frame drops
 	}
@@ -13,8 +26,15 @@ impl GameState {
 	pub fn next_move(&mut self) {
 		match &mut self.game_run {
 			None => !todo!(),
-			Some(ref mut round) => {
-				for state in &mut round.player_states {
+			Some(ref mut game_run) => {
+				// update player position
+				for (x, player) in &mut game_run.level.iter_mut_player().enumerate() {
+					player.position = game_run.player_states[x].position;
+					player.orientation = game_run.player_states[x].orientation;
+				}
+
+				//update next state
+				for state in &mut game_run.player_states {
 					let new_values = getRelativeXY(state);
 					let new_x = state.position.0 as i8 + new_values.0;
 					let new_y = state.position.1 as i8 + new_values.1;
@@ -25,33 +45,61 @@ impl GameState {
 							position: (new_x as u8, new_y as u8),
 							orientation: new_values.2,
 							next_action: state.card_iter.next().unwrap(),
+							rotation: new_values.3,
 							card_iter: state.card_iter.clone()
 						};
 						*state = new_state;
 					}
 				}
-			},
+			}
 		}
 	}
 }
 
-fn getRelativeXY(player_state: &PlayerState) -> (i8, i8, Orientation) {
+fn getRelativeXY(player_state: &PlayerState) -> (i8, i8, Orientation, Rotation) {
 	match &player_state.next_action {
-		None => (0, 0, player_state.orientation),
+		None => (0, 0, player_state.orientation, Rotation {
+			pivot: RotationPoint::NoRotation,
+			sign: 1
+		}),
 		Some(car_action) => {
+			let mut rotation_pivots = [
+				RotationPoint::NoRotation,
+				RotationPoint::NoRotation,
+				RotationPoint::NoRotation,
+				RotationPoint::NoRotation
+			];
+			let mut rotation_sign = 1;
 			let new_orientations = match car_action {
-				CarAction::TurnLeft => [
-					Orientation::West,
-					Orientation::East,
-					Orientation::South,
-					Orientation::North
-				],
-				CarAction::TurnRight => [
-					Orientation::East,
-					Orientation::West,
-					Orientation::North,
-					Orientation::South
-				],
+				CarAction::TurnLeft => {
+					rotation_pivots = [
+						RotationPoint::TopLeft,
+						RotationPoint::BottomRight,
+						RotationPoint::BottomLeft,
+						RotationPoint::TopRight
+					];
+					rotation_sign = -1;
+					[
+						Orientation::West,
+						Orientation::East,
+						Orientation::South,
+						Orientation::North
+					]
+				},
+				CarAction::TurnRight => {
+					rotation_pivots = [
+						RotationPoint::TopRight,
+						RotationPoint::BottomLeft,
+						RotationPoint::BottomRight,
+						RotationPoint::TopLeft
+					];
+					[
+						Orientation::East,
+						Orientation::West,
+						Orientation::North,
+						Orientation::South
+					]
+				},
 				CarAction::DriveForward => [
 					Orientation::North,
 					Orientation::South,
@@ -60,10 +108,22 @@ fn getRelativeXY(player_state: &PlayerState) -> (i8, i8, Orientation) {
 				]
 			};
 			match player_state.orientation {
-				Orientation::North => (-0, -1, new_orientations[0]),
-				Orientation::South => (0, 1, new_orientations[1]),
-				Orientation::West => (-1, 0, new_orientations[2]),
-				Orientation::East => (1, 0, new_orientations[3])
+				Orientation::North => (0, -1, new_orientations[0], Rotation {
+					pivot: rotation_pivots[0],
+					sign: rotation_sign
+				}),
+				Orientation::South => (0, 1, new_orientations[1], Rotation {
+					pivot: rotation_pivots[1],
+					sign: rotation_sign
+				}),
+				Orientation::West => (-1, 0, new_orientations[2], Rotation {
+					pivot: rotation_pivots[2],
+					sign: rotation_sign
+				}),
+				Orientation::East => (1, 0, new_orientations[3], Rotation {
+					pivot: rotation_pivots[3],
+					sign: rotation_sign
+				})
 			}
 		}
 	}
