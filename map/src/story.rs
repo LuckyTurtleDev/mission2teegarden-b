02d7e4099894e4615724b95d275ce60f6347f0fb
LuckyTurtleDev <git@lukas1818.de>
2023-06-16 23:16:33 +0200

    simple test

--- map/src/story.rs
+++ map/src/story.rs
@@ -1,12 +1,17 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub enum Character {}
+pub enum Character {
+	Captain,
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub enum Background {}
+pub enum Background {
+	OuterSpace
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 pub struct Speech {
 	/// The text, which should be show at the botton of the screen.
 	text: String,
@@ -17,7 +22,26 @@ pub struct Speech {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 pub struct Story {
+	#[serde(default)]
 	pub pre_level: Vec<Speech>,
+	#[serde(default)]
 	pub after_level: Vec<Speech>
+}
+
+#[cfg(test)]
+mod tests {
+	use basic_toml as toml;
+    use super::*;
+    
+    #[test]
+    fn load_story() {
+        let toml = r#"
+        [[pre_level]]
+        text = "hi, I am the captain ..."
+		profil = "Captain"
+        "#;
+        let _config: Story = toml::from_str(&toml).unwrap();
+    }
 }
