249fba6e297d95e23b29d9bad47d5f07611c0777
LuckyTurtleDev <git@lukas1818.de>
2023-06-17 19:04:16 +0200

    longer test

--- map/src/story.rs
+++ map/src/story.rs
@@ -1,16 +1,16 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub enum Character {
 	Captain
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub enum Background {
 	OuterSpace
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct Speech {
 	/// The text, which should be show at the botton of the screen.
@@ -21,7 +21,7 @@ pub struct Speech {
 	background: Option<Background>
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct Story {
 	#[serde(default)]
@@ -38,10 +38,38 @@ mod tests {
 	#[test]
 	fn load_story() {
 		let toml = r#"
-        [[pre_level]]
-        text = "hi, I am the captain ..."
+		[[pre_level]]
+		text = "hi, I am the captain ..."
 		profil = "Captain"
-        "#;
-		let _config: Story = toml::from_str(&toml).unwrap();
+		background = "OuterSpace"
+
+		[[pre_level]]
+		text = "now it is you turn!"
+		
+		[[after_level]]
+		text = "You have mastered the challenge!"
+		profil = "Captain"
+		"#;
+		let config: Story = toml::from_str(&toml).unwrap_or_else(|err| panic!("{}", err));
+		let control = Story {
+			pre_level: vec![
+				Speech {
+					text: "hi, I am the captain ...".to_owned(),
+					profil: Some(Character::Captain),
+					background: Some(Background::OuterSpace)
+				},
+				Speech {
+					text: "now it is you turn!".to_owned(),
+					profil: None,
+					background: None
+				},
+			],
+			after_level: vec![Speech {
+				text: "You have mastered the challenge!".to_owned(),
+				profil: Some(Character::Captain),
+				background: None
+			}]
+		};
+		assert_eq!(config, control);
 	}
 }
