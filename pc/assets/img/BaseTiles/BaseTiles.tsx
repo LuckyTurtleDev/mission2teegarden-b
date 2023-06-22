e2f6fd96bc7b3ebce993fcfb50d71b4ea3c36f0c
LuckyTurtleDev <git@lukas1818.de>
2023-06-22 21:07:44 +0200

    new tiles + Rotate tiles (#96)

--- pc/assets/img/BaseTiles/BaseTiles.tsx
+++ pc/assets/img/BaseTiles/BaseTiles.tsx
@@ -1,7 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="grass_corner_sand.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="half_grass_sand.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="sand.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="256" source="sand_corner_grass.png"/>
  </tile>
 </tileset>
