ce3082f6ae27bd197656fb4f03aaae583d7ce9cc
luckyturtledev <git@lukas1818.de>
2023-06-28 18:01:27 +0200

    wip

---
+++ map/src/commands.rs
@@ -0,0 +1,34 @@
+use anyhow::Context;
+use clap::Parser;
+use std::{fs::File, io::Write, path::PathBuf};
+use crate::Map;
+
+#[derive(Debug, Parser)]
+pub struct OptValidateMap {
+	file: String
+}
+
+#[derive(Debug, Parser)]
+pub struct OptExportMap {
+	/// Map to be exported.
+	file: String
+}
+
+pub fn validate(opt: OptValidateMap) -> anyhow::Result<()> {
+	let map = Map::from_tmx(opt.file)?;
+	println!("{map:#?}\nmap is valid");
+	Ok(())
+}
+
+pub fn export(opt: OptExportMap) -> anyhow::Result<()> {
+	let map = Map::from_tmx(&opt.file).context("Failed to load tmx map from file}")?;
+	let path = PathBuf::from(opt.file);
+	let path = path.file_name().context("no filename at path")?;
+	let mut path = PathBuf::from(path);
+	path.set_extension("m2tb_map");
+	println!("export map to {path:?}");
+	let mut file = File::create(&path)
+		.with_context(|| format!("failed to create file {:?}", path))?;
+	write!(file, "{}", map.to_string())
+		.with_context(|| format!("failed to wirte file {:?}", path))
+}
