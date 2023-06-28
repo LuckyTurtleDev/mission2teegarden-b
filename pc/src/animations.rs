881bd71c2b281bde47b2b44d78f19e33fc889bae
Ipagaxi <g.thiesen@posteo.de>
2023-06-28 11:40:52 +0200

    ...

--- pc/src/animations.rs
+++ pc/src/animations.rs
@@ -4,16 +4,19 @@ use macroquad_particles::{AtlasConfig, BlendMode, Emitter, EmitterConfig};
 
 impl GameState {
 	pub(crate) async fn load_fire_emitter(&mut self) {
-		let texture = load_texture("assets/img/Animations/crash_fire.png")
+		let texture = load_texture("assets/img/Animations/crash_fire_2.png")
 			.await
 			.unwrap();
 		let emitter = Emitter::new(EmitterConfig {
 			local_coords: false,
 			texture: Some(texture),
-			lifetime: 0.5,
-			amount: 3,
-			atlas: Some(AtlasConfig::new(4, 4, 12..)),
-			size: 20.0,
+			lifetime: 0.8,
+			lifetime_randomness: 0.7,
+			explosiveness: 0.95,
+			initial_direction_spread: 0.5,
+			amount: 20,
+			atlas: Some(AtlasConfig::new(4, 4, 0..8)),
+			size: 15.0,
 			blend_mode: BlendMode::Additive,
 			..Default::default()
 		});
