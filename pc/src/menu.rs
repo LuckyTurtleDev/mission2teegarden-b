05b8b734172a411fb71254e1cb0db03bd8453bf2
Georg Thiesen <g.thiesen@posteo.de>
2023-07-05 00:58:09 +0200

    WIP: Menu (#104)
    * added temp menu
    
    * added unfinished level menu
    
    * fixed: level works now somehow
    
    * restructured game loop
    
    * fixed frame drop in menu activities
    
    * trying to integrate pybadge menu control
    
    * menu without imgs
    
    * fix
    
    * added first implementation of pybadge menu control
    
    * pybadge can control menu now, but panics after selection
    
    * tried menu background
    
    * added menu background img
    
    * fmt
    
    * added available cards to levels
    
    * menus dimensions are now dynamic
    
    * level menu is scaled dynamically too but the hole menu is lagging
    
    * fixed some merge issues
    
    * ...

--- pc/src/menu.rs
+++ pc/src/menu.rs
@@ -4,54 +4,66 @@ use mission2teegarden_b_models::{AvailableCards, Key, ToPcGameEvent, ToPypadeGam
 use macroquad::{
 	hash,
 	prelude::*,
-	ui::{root_ui, Skin}
+	ui::{root_ui, widgets, Skin}
 };
 
 const BUTTON_FONT_SIZE: u16 = 16;
 
-fn get_button_skin() -> Skin {
-	{
-		let window_style = root_ui()
-			.style_builder()
-			.background_margin(RectOffset::new(20.0, 20.0, 10.0, 10.0))
-			.margin(RectOffset::new(-20.0, -30.0, 0.0, 0.0))
-			.build();
-		let button_style = root_ui()
-			.style_builder()
-			.background(Image::from_file_with_format(
-				include_bytes!("../assets/img/Menu/button_background.png"),
-				None
-			))
-			.background_margin(RectOffset::new(37.0, 37.0, 5.0, 5.0))
-			.margin(RectOffset::new(10.0, 10.0, 0.0, 0.0))
-			.text_color(Color::from_rgba(180, 180, 100, 255))
-			.font_size(BUTTON_FONT_SIZE)
-			.build();
-		Skin {
-			window_style,
-			button_style,
-			..root_ui().default_skin()
-		}
+fn get_button_font_size(container_height: f32) -> u16 {
+	let font_size = 20;
+	let max_text = "###########";
+	let text_dim = measure_text(max_text, None, font_size, 1.0);
+	let relative_width =
+		(container_height * 0.75) / (text_dim.height + text_dim.offset_y);
+	font_size * relative_width as u16
+}
+
+fn get_button_skin(font_size: u16) -> Skin {
+	let window_style = root_ui()
+		.style_builder()
+		.background_margin(RectOffset::new(20.0, 20.0, 10.0, 10.0))
+		.margin(RectOffset::new(-20.0, -30.0, 0.0, 0.0))
+		.build();
+	let button_style = root_ui()
+		.style_builder()
+		.background(Image::from_file_with_format(
+			include_bytes!("../assets/img/Menu/button_background.png"),
+			None
+		))
+		.background_margin(RectOffset::new(37.0, 37.0, 5.0, 5.0))
+		.margin(RectOffset::new(10.0, 10.0, 0.0, 0.0))
+		.text_color(Color::from_rgba(180, 180, 100, 255))
+		.font_size(font_size)
+		.build();
+	Skin {
+		window_style,
+		button_style,
+		..root_ui().default_skin()
 	}
 }
 
-fn get_button_focused_skin() -> Skin {
-	{
-		let button_style = root_ui()
-			.style_builder()
-			.background(Image::from_file_with_format(
-				include_bytes!("../assets/img/Menu/button_focused_background.png"),
-				None
-			))
-			.background_margin(RectOffset::new(37.0, 37.0, 5.0, 5.0))
-			.margin(RectOffset::new(10.0, 10.0, 0.0, 0.0))
-			.text_color(Color::from_rgba(180, 180, 100, 255))
-			.font_size(BUTTON_FONT_SIZE)
-			.build();
-		Skin {
-			button_style,
-			..root_ui().default_skin()
-		}
+fn get_button_focused_skin(font_size: u16) -> Skin {
+	let button_style = root_ui()
+		.style_builder()
+		.background(Image::from_file_with_format(
+			include_bytes!("../assets/img/Menu/button_focused_background.png"),
+			None
+		))
+		.build();
+	let button_style = root_ui()
+		.style_builder()
+		.background(Image::from_file_with_format(
+			include_bytes!("../assets/img/Menu/button_focused_background.png"),
+			None
+		))
+		.background_margin(RectOffset::new(37.0, 37.0, 5.0, 5.0))
+		.margin(RectOffset::new(10.0, 10.0, 0.0, 0.0))
+		.text_color(Color::from_rgba(180, 180, 100, 255))
+		.font_size(font_size)
+		.build();
+	Skin {
+		button_style,
+		..root_ui().default_skin()
 	}
 }
 
@@ -81,53 +93,90 @@ impl GameState {
 		let background_texture = load_texture("assets/img/Menu/menu_background.png")
 			.await
 			.unwrap();
-		let screen_width = screen_width();
-		let screen_height = screen_height();
-		let menu_size = vec2(screen_width * 0.5, screen_height * 0.8);
-		let menu_position = vec2(
-			(screen_width - menu_size.x) / 2.0,
-			(screen_height - menu_size.y) / 2.0
-		);
 		let mut button_focused_index = 0;
-
-		let button_skin = get_button_skin();
-		let button_focused_skin = get_button_focused_skin();
-		let mut skin_1 = &button_focused_skin.clone();
-		let mut skin_2 = &button_skin.clone();
 		let mut enter_pressed = false;
-		root_ui().push_skin(&skin_1);
+
 		while self.activity == Activity::Menu && self.running {
 			let events = self.input_players.get_events();
+			let screen_width = screen_width();
+			let screen_height = screen_height();
+			/*let menu_position = vec2(
+				(screen_width - menu_size.x) / 2.0,
+				(screen_height - menu_size.y) / 2.0
+			);*/
+			let button_size = vec2(screen_width / 3.0, screen_height / 10.0);
+			let font_size = get_button_font_size(button_size.y);
+			let button_skin = get_button_skin(font_size);
+			let button_focused_skin = get_button_focused_skin(font_size);
+			let mut skin_1 = &button_focused_skin.clone();
+			let mut skin_2 = &button_skin.clone();
+
+			root_ui().push_skin(&skin_1);
+			let relative_size = (screen_width / background_texture.width())
+				.max(screen_height / background_texture.height());
+			let background_dim = vec2(
+				relative_size * background_texture.width(),
+				relative_size * background_texture.height()
+			);
+			let draw_params = DrawTextureParams {
+				dest_size: Some(background_dim),
+				..Default::default()
+			};
+			draw_texture_ex(
+				background_texture,
+				-((background_dim.x - screen_width) / 2.0),
+				-((background_dim.y - screen_height) / 2.0),
+				WHITE,
+				draw_params
+			);
 			button_focused_index = (button_focused_index
 				+ evaluate_events(&events, &mut enter_pressed))
 			.clamp(0, 1);
-			draw_texture(background_texture, 0.0, 0.0, WHITE);
-			root_ui().window(hash!(), menu_position, menu_size, |ui| {
-				ui.pop_skin();
-				if button_focused_index == 0 {
-					if enter_pressed {
+			// It does not work when group has same size as screen
+			root_ui().group(
+				hash!(),
+				vec2(screen_width - 1.0, screen_height - 1.0),
+				|ui| {
+					ui.pop_skin();
+					if button_focused_index == 0 {
+						if enter_pressed {
+							self.activity = Activity::SelectLevel;
+						}
+						skin_1 = &button_focused_skin;
+						skin_2 = &button_skin;
+					} else {
+						if enter_pressed {
+							self.running = false;
+						}
+						skin_1 = &button_skin;
+						skin_2 = &button_focused_skin;
+					}
+					ui.push_skin(&skin_1);
+
+					let play_button = widgets::Button::new("Play")
+						.position(vec2(
+							(screen_width - button_size.x) / 2.0,
+							(screen_height - button_size.y) / 2.0 - 1.5 * button_size.y
+						))
+						.size(button_size)
+						.ui(ui);
+					if play_button {
 						self.activity = Activity::SelectLevel;
 					}
-					skin_1 = &button_focused_skin;
-					skin_2 = &button_skin;
-				} else {
-					if enter_pressed {
+					ui.pop_skin();
+					ui.push_skin(&skin_2);
+					let quit_button = widgets::Button::new("Quit")
+						.position(vec2(
+							(screen_width - button_size.x) / 2.0,
+							(screen_height - button_size.y) / 2.0 + 1.5 * button_size.y
+						))
+						.size(button_size)
+						.ui(ui);
+					if quit_button {
 						self.running = false;
 					}
-					skin_1 = &button_skin;
-					skin_2 = &button_focused_skin;
 				}
-				ui.push_skin(&skin_1);
-
-				if ui.button(vec2(140.0, 100.0), "Play") {
-					self.activity = Activity::SelectLevel;
-				}
-				ui.pop_skin();
-				ui.push_skin(&skin_2);
-				if ui.button(vec2(140.0, 200.0), "Quit") {
-					self.running = false;
-				}
-			});
+			);
 
 			next_frame().await;
 		}
@@ -137,32 +186,73 @@ impl GameState {
 		let background_texture = load_texture("assets/img/Menu/menu_background.png")
 			.await
 			.unwrap();
-		let screen_width = screen_width();
-		let screen_height = screen_height();
-		let menu_size = vec2(screen_width * 0.5, screen_height * 0.8);
-		let menu_position = vec2(
-			(screen_width - menu_size.x) / 2.0,
-			(screen_height - menu_size.y) / 2.0
-		);
-		let button_skin = get_button_skin();
-		let button_focused_skin = get_button_focused_skin();
 		let mut enter_pressed = false;
 		let mut button_focused_index = 0;
 
 		while self.activity == Activity::SelectLevel && self.running {
 			let events = self.input_players.get_events();
+			let screen_width = screen_width();
+			let screen_height = screen_height();
+			let button_size = vec2(screen_width / 3.0, screen_height / 10.0);
+			let font_size = get_button_font_size(button_size.y);
+			let level_wrapper_height = screen_height * 0.7;
+			let wrapper_offset = (screen_height - level_wrapper_height) / 2.0;
+			let button_offset = level_wrapper_height / (LEVELS.len() + 1) as f32;
+			let button_skin = get_button_skin(font_size);
+			let button_focused_skin = get_button_focused_skin(font_size);
 			button_focused_index = (button_focused_index
 				+ evaluate_events(&events, &mut enter_pressed))
 			.clamp(0, LEVELS.len() as i8);
-			draw_texture(background_texture, 0.0, 0.0, WHITE);
-			root_ui().window(hash!(), menu_position, menu_size, |ui| {
-				for (i, _level) in LEVELS.clone().into_iter().enumerate() {
-					ui.pop_skin();
-					if button_focused_index == i as i8 {
-						if enter_pressed {
-							self.level_num = i;
+			let relative_size = (screen_width / background_texture.width())
+				.max(screen_height / background_texture.height());
+			let background_dim = vec2(
+				relative_size * background_texture.width(),
+				relative_size * background_texture.height()
+			);
+			let draw_params = DrawTextureParams {
+				dest_size: Some(background_dim),
+				..Default::default()
+			};
+			draw_texture_ex(
+				background_texture,
+				-((background_dim.x - screen_width) / 2.0),
+				-((background_dim.y - screen_height) / 2.0),
+				WHITE,
+				draw_params
+			);
+			root_ui().group(
+				hash!(),
+				vec2(screen_width - 1.0, screen_height - 1.0),
+				|ui| {
+					for (i, _level) in LEVELS.clone().into_iter().enumerate() {
+						ui.pop_skin();
+						if button_focused_index == i as i8 {
+							if enter_pressed {
+								self.level_num = i;
+								self.activity = Activity::GameRound(Phase::Introduction);
+								self.sound_player.play_level_music();
+							}
+							let skin = &button_focused_skin.clone();
+							ui.push_skin(skin);
+						} else {
+							let skin = &button_skin.clone();
+							ui.push_skin(skin);
+						}
+						let level_button =
+							widgets::Button::new(format!("Level {}", i + 1))
+								.position(vec2(
+									(screen_width - button_size.x) / 2.0,
+									wrapper_offset + i as f32 * button_offset
+								))
+								.size(button_size)
+								.ui(ui);
+						if level_button {
 							self.activity = Activity::GameRound(Phase::Introduction);
-							self.sound_player.play_level_music();
+						}
+					}
+					if button_focused_index == LEVELS.len() as i8 {
+						if enter_pressed {
+							self.activity = Activity::Menu;
 						}
 						let skin = &button_focused_skin.clone();
 						ui.push_skin(skin);
@@ -170,27 +260,30 @@ impl GameState {
 						let skin = &button_skin.clone();
 						ui.push_skin(skin);
 					}
-					if ui.button(vec2(140.0, i as f32 * 50.0), format!("Level {}", i + 1))
-					{
-						self.activity = Activity::GameRound(Phase::Introduction);
-					}
-				}
 
-				ui.pop_skin();
-				if button_focused_index == LEVELS.len() as i8 {
-					if enter_pressed {
+					ui.pop_skin();
+					if button_focused_index == LEVELS.len() as i8 {
+						if enter_pressed {
+							self.activity = Activity::Menu;
+						}
+						let skin = &button_focused_skin.clone();
+						ui.push_skin(skin);
+					} else {
+						let skin = &button_skin.clone();
+						ui.push_skin(skin);
+					}
+					let back_button = widgets::Button::new("Back")
+						.position(vec2(
+							(screen_width - button_size.x) / 2.0,
+							wrapper_offset + LEVELS.len() as f32 * button_offset
+						))
+						.size(button_size)
+						.ui(ui);
+					if back_button {
 						self.activity = Activity::Menu;
 					}
-					let skin = &button_focused_skin.clone();
-					ui.push_skin(skin);
-				} else {
-					let skin = &button_skin.clone();
-					ui.push_skin(skin);
 				}
-				if ui.button(vec2(140.0, LEVELS.len() as f32 * 50.0), "Back") {
-					self.activity = Activity::Menu;
-				}
-			});
+			);
 			next_frame().await;
 		}
 	}
