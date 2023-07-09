6eea2dc696d0a9750cad9f76cdf965e817b2747a
LuckyTurtleDev <git@lukas1818.de>
2023-07-09 20:10:22 +0200

    fix #199 must check how it looks like at the pybadge. have none at the moment

--- pybadge/src/activitys/game_over.rs
+++ pybadge/src/activitys/game_over.rs
@@ -13,9 +13,9 @@ pub(crate) fn init(
 		.draw(&mut state.display)
 		.unwrap();
 	let (text, x) = match game_over_type {
-		GameOver::DriveAway => (" You have moved into\n unknown terrain and\nwere never seen again", 20),
-		GameOver::TimeOut => ("You have not reach\nyour goal in time", 25),
-		GameOver::Crash => ("          Kaboom!\n\n      You have a crash\n      with you car.\n\nSomething smells burnt ...",2)
+		GameOver::DriveAway => (" You robot have moved into\n unknown terrain and\nlost connection", 20),
+		GameOver::TimeOut => ("Your robot has successfully\n completed its task", 25),
+		GameOver::Crash => ("          Kaboom!\n\n    You robot have crashed!\n looks like it will not work anymore",2)
 	};
 	Text::new(text, Point::new(x, 60), state.text_style)
 		.draw(&mut state.display)
