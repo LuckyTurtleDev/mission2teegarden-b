26958d2e8b7d5910da7bf75b90d12d78450da6ba
luckyturtledev <git@lukas1818.de>
2023-07-11 20:54:47 +0200

    add waiting activity

---
+++ pybadge/src/activitys/waiting.rs
@@ -0,0 +1,15 @@
+use crate::State;
+use embedded_graphics::{prelude::*, text::Text};
+use mission2teegarden_b_models::GameOver;
+use pybadge_high::Color;
+
+pub(crate) fn init(state: &mut State<'_>) {
+	state.display.clear(Color::BLACK).unwrap();
+	Text::new(
+		"look at the pc screen",
+		Point::new(15, 50),
+		state.text_style
+	)
+	.draw(&mut state.display)
+	.ok();
+}
