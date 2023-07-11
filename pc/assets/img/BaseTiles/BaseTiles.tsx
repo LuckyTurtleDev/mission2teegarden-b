2923996254916737354ac89420779437ba151076
Laura Rolfes <rolfeslaura@gmail.com>
2023-07-11 17:47:58 +0200

    Levels
    Finished the rest of the levels, can add some more between level 5 and 6 or between 6 and 7(should be possible without much story connflicting with it)

--- pc/assets/img/BaseTiles/BaseTiles.tsx
+++ pc/assets/img/BaseTiles/BaseTiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="5" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="grass.png"/>
@@ -15,5 +15,26 @@
  </tile>
  <tile id="4">
   <image width="256" height="256" source="sand_corner_grass.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="grass_double_corner_sand.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="256" source="sand_double_corner_grass.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="256" source="strange_grass.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="strange_grass_corner.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="256" source="strange_grass_double_corner.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="256" source="strange_grass_half.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="256" source="strange_grass_inner_corner.png"/>
  </tile>
 </tileset>
