95e6de52fb4e1c1d35e527650733ffb9b8d6f7da
Laura R <103964513+AtlasNr3@users.noreply.github.com>
2023-07-12 12:42:15 +0000

    fix4 rustfmt

--- pc/src/assets.rs
+++ pc/src/assets.rs
@@ -84,8 +84,7 @@ pub(crate) static LEVELS: Lazy<Vec<&str>> = Lazy::new(|| {
 		include_map!("pc/assets/level/007.tmx"),
 		include_map!("pc/assets/level/008.tmx"),
 		include_map!("pc/assets/level/009.tmx"),
-		include_map!("pc/assets/level/010.tmx")
-
+		include_map!("pc/assets/level/010.tmx"), 
 	]
 });
 
