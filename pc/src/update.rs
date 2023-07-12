d0771c2ee6de276554011f9f759f4bf028a0d697
Ipagaxi <g.thiesen@posteo.de>
2023-07-12 09:16:30 +0200

    ...

--- pc/src/update.rs
+++ pc/src/update.rs
@@ -138,6 +138,13 @@ impl GameState {
 		false
 	}
 
+	pub(crate) fn reset_pybadges(&self) {
+		for player in self.input_players.players.iter().flatten() {
+			todo!("s")
+			//player.send_events(ToPypadeGameEvent::Reset);
+		}
+	}
+
 	/// calculate next moves
 	pub(crate) fn next_move(&mut self) {
 		if let Some(ref mut game_run) = self.game_run {
