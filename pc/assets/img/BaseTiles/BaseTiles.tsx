4587e71aabdd88562171ad3b86c7971ef9d46c23
Laura Rolfes <rolfeslaura@gmail.com>
2023-07-12 19:30:35 +0200

    the other 5 levels
    and minor changes

--- pc/assets/img/BaseTiles/BaseTiles.tsx
+++ pc/assets/img/BaseTiles/BaseTiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="11" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="grass.png"/>
@@ -33,5 +33,8 @@
  </tile>
  <tile id="10">
   <image width="256" height="256" source="../../../../../../../../OneDrive/Dokumente/Unizeug/SS23/Praktikum/Grafiken/strange_grass_inner_corner.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="256" source="strange_grass_half.png"/>
  </tile>
 </tileset>
