c30fb1616014f263f0b27d2d9888cd55b89a3512
Laura Rolfes <rolfeslaura@gmail.com>
2023-07-12 17:27:11 +0200

    New Tiles
    New Base and Object Tiles nochmal neu hochgeladen

--- pc/assets/img/BaseTiles/BaseTiles.tsx
+++ pc/assets/img/BaseTiles/BaseTiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="5" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="BaseTiles" tilewidth="256" tileheight="256" tilecount="11" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="grass.png"/>
@@ -15,5 +15,23 @@
  </tile>
  <tile id="4">
   <image width="256" height="256" source="sand_corner_grass.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="../../../../../../../../OneDrive/Dokumente/Unizeug/SS23/Praktikum/Grafiken/grass_double_corner_sand.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="256" source="../../../../../../../../OneDrive/Dokumente/Unizeug/SS23/Praktikum/Grafiken/sand_double_corner_grass.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="256" source="../../../../../../../../OneDrive/Dokumente/Unizeug/SS23/Praktikum/Grafiken/strange_grass.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="../../../../../../../../OneDrive/Dokumente/Unizeug/SS23/Praktikum/Grafiken/strange_grass_corner.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="256" source="../../../../../../../../OneDrive/Dokumente/Unizeug/SS23/Praktikum/Grafiken/strange_grass_double_corner.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="256" source="../../../../../../../../OneDrive/Dokumente/Unizeug/SS23/Praktikum/Grafiken/strange_grass_inner_corner.png"/>
  </tile>
 </tileset>
